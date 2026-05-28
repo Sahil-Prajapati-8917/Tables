5a0d967873dcd589562826802a8eb046b05e7ba9
Sahil-Prajapati-8917 <sahilprajapati8917@gmail.com>
2026-05-28 14:59:50 +0530

    refactor: relax type constraints for ClassValue function arguments using any

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-type ClassValue = string | ((...args: unknown[]) => unknown) | undefined | null | false
+type ClassValue = string | ((...args: any[]) => any) | undefined | null | false
 
 export function cn(...classes: ClassValue[]): string {
   return classes.filter((c): c is string => typeof c === 'string').join(' ')
