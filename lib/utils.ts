da90209364774f4814de221f7cc6c7bb7e295505
Sahil-Prajapati-8917 <sahilprajapati8917@gmail.com>
2026-05-30 18:53:29 +0530

    refactor: update cn utility to support functional class values via ClassValue type definition

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
-export function cn(...classes: (string | undefined | null | false)[]): string {
+type ClassValue = string | ((...args: never[]) => string | undefined | null | false) | undefined | null | false
+
+export function cn(...classes: ClassValue[]): string {
   return classes.filter((c): c is string => typeof c === 'string').join(' ')
 }
